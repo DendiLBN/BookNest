bdd2f94cff2dc19e9eee9b331a74304be5d7e340
Damian <124635785+DendiLBN@users.noreply.github.com>
2026-05-07 19:17:50 +0200

    Add responsive design tokens

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import "@/assets/global-styles/tokens.css";
+
 import { store } from "@/store/index";
 // TODO IGNORE BELOW import
 import App from "./App";
